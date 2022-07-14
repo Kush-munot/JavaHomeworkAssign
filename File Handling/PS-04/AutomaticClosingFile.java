bda8753917ae95e3a8984b304ae0a54ac26bf340
Kush-munot <munot.kgm@gmail.com>
2022-07-14 14:58:45 +0530

    /

---
+++ File Handling/PS-04/AutomaticClosingFile.java
@@ -0,0 +1,17 @@
+import java.time.chrono.MinguoChronology;
+
+import java.io.*;
+class AutomaticClosingFile{
+    public static void main(String[] args) {
+        try(FileInputStream f = new FileInputStream("./file.txt")){
+            int i = 0;
+            String s="";
+            while((i=f.read())!= -1){
+                s+=(char)i;
+            }
+            System.out.println(s);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
