e11e40286a77b5fe441ae264e110477fae4293fd
Kush-munot <munot.kgm@gmail.com>
2022-07-15 08:56:21 +0530

    .

--- Generics/GenericMethodTest.java
+++ Generics/GenericMethodTest.java
@@ -1,12 +1,9 @@
 
-public class GenericMethodTest
-{   
-   public static < E > void printArray( E[] inputArray )
-   {
-            for ( E element : inputArray )
-           {        
-                        System.out.printf( "%s ", element );
-            }
-         System.out.println();
-    }
+public class GenericMethodTest {
+   public static <E> void printArray(E[] inputArray) {
+      for (E element : inputArray) {
+         System.out.printf("%s ", element);
+      }
+      System.out.println();
+   }
 }
