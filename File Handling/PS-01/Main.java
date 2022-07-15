9da6ae8ac7c8634a58ce6854423b84fa50e2d6c7
Kush-munot <munot.kgm@gmail.com>
2022-07-15 08:29:48 +0530

    .

--- File Handling/PS-01/Main.java
+++ File Handling/PS-01/Main.java
@@ -1,26 +1,28 @@
 import java.io.*;
 
-class Main{
+class Main {
     public static void main(String[] args) {
-        BufferedReader r = null;
-        BufferedWriter w = null;
 
-        try{
-            r = new BufferedReader(new FileReader("file.txt"));
-            w = new BufferedWriter(new FileWriter("out.txt"));
-            int space=0,words=0,chars=0,lines=0;
-            String line ;
-            while((line = r.readLine())!=null){
+        try (BufferedReader r = new BufferedReader(new FileReader("file.txt"));
+                BufferedWriter w = new BufferedWriter(new FileWriter("out.txt"));) {
+
+            int space = 0, words = 0, chars = 0, lines = 0;
+            String line;
+            while ((line = r.readLine()) != null) {
                 lines++;
-                for(int i=0;i<line.length();i++){
-                    if(line.charAt(i) == ' ')
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ' ')
                         space++;
-                    if((line.charAt(i)>='a' && line.charAt(i)<='z') || (line.charAt(i)>='A' && line.charAt(i)<='Z'))
+                    if ((line.charAt(i) >= 'a' && line.charAt(i) <= 'z')
+                            || (line.charAt(i) >= 'A' && line.charAt(i) <= 'Z'))
                         chars++;
                 }
             }
-            words = space+1;
-            System.out.println("Characters = " + chars + " Words = " + words + " Lines = " + lines + " Spaces = "+ space);
-        }catch(IOException e){System.out.println(e);}
+            words = space + 1;
+            System.out.println(
+                    "Characters = " + chars + " Words = " + words + " Lines = " + lines + " Spaces = " + space);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
