e44a6bd3c7ed82b5904a1761712c1370fd0f3234
Kush-munot <munot.kgm@gmail.com>
2022-07-31 21:58:18 +0530

    .

---
+++ Exam Questions/voterId.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class voterId {
+	public static void main(String[] args) {
+		String name, dobs, city;
+		int day,month,year;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name: ");
+		name = sc.nextLine();
+		System.out.println("Enter city: ");
+		city = sc.nextLine();
+		System.out.println("Enter date of birth's date: dd/mm/yy ");
+		dobs = sc.nextLine();
+		String arr[] = dobs.split("/");
+		day = Integer.parseInt(arr[0]);
+		month = Integer.parseInt(arr[1]);
+		year = Integer.parseInt(arr[2]);
+
+		LocalDate dob =  LocalDate.of(year,month,day);
+		LocalDate curD = java.time.LocalDate.now();
+
+		Period period = Period.between(dob, curD);
+		int diff = period.getYears();
+		System.out.println(diff);
+
+	}
+}
